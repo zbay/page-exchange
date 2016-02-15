b9652387e8d8199f9ccd093cd18c14c3636113cb
zbay <zlwilliams1@gmail.com>
2016-02-15 07:14:37 +0000

    Mostly finished a page for accepted trades

--- controllers/trades.js
+++ controllers/trades.js
@@ -79,6 +79,44 @@ app.get("/proposeTrade/:id", requireLogin, function(req, res){
     });
 });
 
+app.get("/acceptedTrades", function(req, res){
+    var acceptedTrades = [];
+    Trade.find({$or: [{"proposerID": req.session.sessionID}, {"proposeeID": req.session.sessionID}]}, function(err, tradeData){
+        for(var i = 0; i < tradeData.length; i++){
+            var searchThisID;
+            var receiveID;
+            var giveID;
+            if(tradeData.proposerID == req.session.sessionID){ //the proposer was the user
+                searchThisID = tradeData.proposeeID;
+                receiveID = tradeData.proposeeBookID;
+                giveID = tradeData.proposerBookID;
+            }
+            else{ //the proposer was the other user
+                searchThisID = tradeData.proposerID;
+                receiveID = tradeData.proposerBookID;
+                giveID = tradeData.proposerBookID;
+            }
+            User.find({"_id": searchThisID}, function(err, partnerData){ //REPLACE SEARCHTHISID, RECEIVEID, and GIVEID WITH TERNARY OPERATORS
+                console.log("partnerData: " + partnerData);
+                Book.find({"_id": receiveID}, function(err, receivedBookData){
+                    Book.find({"_id": giveID}, function(err, givenBookData){
+                        acceptedTrades.push({"toGive": "\"" + givenBookData.title + "\"" + " by " + givenBookData.author, "toReceive":
+                            "\"" + receivedBookData.title + "\"" + " by " + receivedBookData.author, "trader": {"name": partnerData.name, 
+                            "email": partnerData.email, "location": partnerData.city + ", " + partnerData.region}
+                        });
+                        if(i == tradeData.length-1){
+                            res.render("acceptedTrades", {"acceptedTrades": acceptedTrades});
+                        }
+                    });
+                });
+            });
+        }
+        if(err || !tradeData.length){
+            res.render("acceptedTrades");
+        }
+    });
+});
+
 app.post("/initiateTrade", requireLogin, function(req, res){
     var newTrade = new Trade({"proposerID": req.session.sessionID, "proposeeBookID": req.body.bookReceived, "proposerBookID": req.body.bookGiven, "proposeeID": req.body.tradePartner});
     newTrade.save(function(err, message){
